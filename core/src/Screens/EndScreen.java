d0c67ccf457a826f700db2c3ef662bb73125b7f0
Landon Patmore <landon.patmore@gmail.com>
2018-02-02 12:48:56 -0500

    added end game screen boilerplate

---
+++ core/src/Screens/EndScreen.java
@@ -0,0 +1,60 @@
+package Screens;
+
+import Actors.Player;
+import Utils.Globals;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class EndScreen implements Screen {
+
+    private SpriteBatch batch;
+    private BitmapFont font;
+
+    public EndScreen(boolean won) {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        batch.begin();
+        font.draw(batch, String.valueOf("Game Over"), Globals.width / 2, Globals.height / 2);
+        font.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
